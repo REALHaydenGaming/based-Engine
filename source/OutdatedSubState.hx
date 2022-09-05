149c775fcc829d075844d03d6d004d7a116c34cd
Not_HaydenGaming <107285739+REALHaydenGaming@users.noreply.github.com>
2022-09-05 10:25:00 -0400

    industrual society, our future.

---
+++ source/OutdatedSubState.hx
@@ -0,0 +1,44 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+	var changelogBased:String = "look down gay ass.";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var ver = "v" + MainMenuState.engineVersion;
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"Hey dude! You are playing an outdated build of based Engine!\n\n
+			Please download the LATEST build on GitHub!\nThank's for playing
+			This mod!",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			FlxG.openURL("github.com/REALHaydenGaming");
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
